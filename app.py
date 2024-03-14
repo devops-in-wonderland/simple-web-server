ee8e0b18e939e0cfd27bb70b195ef0143f7f7fe6
Erdinç Akgün <erdinc.akgun@icloud.com>
2024-03-14 21:29:54 +0300

    fix hardcoded path

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import time
 app = Flask(__name__)
 
 app.config['MAX_CONTENT_LENGTH'] = 32 * 1024 * 1024
-app.config['UPLOAD_PATH'] = os.path.join('uploads')
+app.config['UPLOAD_PATH'] = 'uploads'
 
 
 def mkdir_if_does_not_exists(dir: str):
@@ -16,32 +16,33 @@ def mkdir_if_does_not_exists(dir: str):
 mkdir_if_does_not_exists(app.config['UPLOAD_PATH'])
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
-    if request.method == 'POST':
-        uploaded_file = request.files['file']
-        if uploaded_file.filename == 'index.html':
-
-            timestamp_text = str(int(time.time() * 1e3))
-
-            new_file_dir = os.path.join(
-                app.config['UPLOAD_PATH'], timestamp_text)
-
-            mkdir_if_does_not_exists(new_file_dir)
-
-            new_file_path = os.path.join(new_file_dir, uploaded_file.filename)
-
-            uploaded_file.save(new_file_path)
-
-            return timestamp_text
-        else:
-            return Response(status=405)
     return render_template('index.html')
 
+@app.route('/upload', methods=['POST'])
+def index():
+    uploaded_file = request.files['file']
+    if uploaded_file.filename == 'index.html':
+
+        timestamp_text = str(int(time.time() * 1e3))
+
+        new_file_dir = os.path.join(
+            app.config['UPLOAD_PATH'], timestamp_text)
+
+        mkdir_if_does_not_exists(new_file_dir)
+
+        new_file_path = os.path.join(new_file_dir, uploaded_file.filename)
+
+        uploaded_file.save(new_file_path)
+
+        return timestamp_text
+    else:
+        return Response(status=405)
 
 @app.route('/test-results/<folder>')
 def get_results(folder):
-    with open(f'test-results/{str(folder)}/index.html') as f:
+    with open(f'{app.config['UPLOAD_PATH']}/{str(folder)}/index.html') as f:
         content = f.read()
     return content
 
